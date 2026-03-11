99c20e436aeddccadb3e0c2f2a5dd84fce9b4527
Tova <tova.bryngelsson@live.se>
2026-03-11 09:43:37 +0100

    /users from /user in path

--- src/main/java/com/example/socapplication/controller/AppUserController.java
+++ src/main/java/com/example/socapplication/controller/AppUserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("api/users")
 public class AppUserController {
     private final CurrentUser currentUser;
 
