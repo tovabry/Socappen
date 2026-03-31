6fac379f2f272c51104abdb08984df561b10ce65
Tova <tova.bryngelsson@live.se>
2026-03-31 10:35:10 +0200

    faq by id

--- src/main/java/com/example/socapplication/controller/FAQController.java
+++ src/main/java/com/example/socapplication/controller/FAQController.java
@@ -4,13 +4,14 @@ import com.example.socapplication.model.dto.faqDto.ResponseFaq;
 import com.example.socapplication.service.CurrentUser;
 import com.example.socapplication.service.FAQService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/faq")
+@RequestMapping("/api/faq")
 public class FAQController {
 
     FAQService faqService;
@@ -19,7 +20,10 @@ public class FAQController {
         this.faqService = faqService;
     }
 
-    @GetMapping("/all")
+    @GetMapping("/{id}")
+    public ResponseFaq getFaqById(@PathVariable Long id) {return faqService.findFaqById(id);}
+
+    @GetMapping
     public List<ResponseFaq> getAllFaqs(){
         return faqService.findAll();
     }
