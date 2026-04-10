1dedd9a151965e7bc5fa3b44a3a21624859e81f6
Tova <tova.bryngelsson@live.se>
2026-04-10 14:33:59 +0200

    post_log and faq_log

--- src/main/resources/db/migration/V5__logtable.sql
+++ src/main/resources/db/migration/V5__logtable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE auth_log (
                           id           BIGINT PRIMARY KEY IDENTITY(1,1),
-                          email        NVARCHAR(255) NOT NULL,
+                      /*    email        NVARCHAR(255) NOT NULL, */
                           ip_address   VARCHAR(45) NOT NULL,
                           success      BIT NOT NULL,
                           fail_reason  VARCHAR(50),
@@ -28,5 +28,26 @@ CREATE TABLE audit_log (
     ip_address VARCHAR(45),
     reason VARCHAR(MAX),
     created_at DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET()
+)
+GO
+
+CREATE TABLE post_log (
+    id BIGINT PRIMARY KEY IDENTITY(1,1),
+    app_user_id BIGINT NOT NULL,
+    post_id BIGINT NOT NULL,
+    ip_address VARCHAR(45),
+    created_at DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET()
+)
+GO
+
+CREATE TABLE faq_log (
+    id BIGINT PRIMARY KEY IDENTITY(1,1),
+    app_user_id BIGINT NOT NULL,
+    faq_id BIGINT NOT NULL,
+    ip_address VARCHAR(45),
+    created_at DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET()
+
+        CONSTRAINT FK_faqlog_user FOREIGN KEY (app_user_id) REFERENCES app_user(id),
+        CONSTRAINT FK_faglog_faq FOREIGN KEY (faq_id) REFERENCES frequently_asked_question(id)
 )
 GO
