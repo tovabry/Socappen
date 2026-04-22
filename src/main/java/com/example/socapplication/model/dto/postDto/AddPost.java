c9ab5e6b4042f037826ba47c3469c4638e475b6c
Tova <tova.bryngelsson@live.se>
2026-04-22 09:29:58 +0200

    post media in dto

--- src/main/java/com/example/socapplication/model/dto/postDto/AddPost.java
+++ src/main/java/com/example/socapplication/model/dto/postDto/AddPost.java
@@ -1,8 +1,13 @@
 package com.example.socapplication.model.dto.postDto;
 
+import com.example.socapplication.model.dto.postMediaDto.AddPostMedia;
+
+import java.util.List;
+
 public record AddPost(
         Long userId,
         String title,
-        String content
+        String content,
+        List<AddPostMedia> media
 ) {
 }
