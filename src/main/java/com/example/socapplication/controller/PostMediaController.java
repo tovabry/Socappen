2c156e04d5adbd593d0ea666a100e99b217b671b
Tova <tova.bryngelsson@live.se>
2026-04-29 11:35:29 +0200

    Admin permission on post media

--- src/main/java/com/example/socapplication/controller/PostMediaController.java
+++ src/main/java/com/example/socapplication/controller/PostMediaController.java
@@ -6,6 +6,7 @@ import com.example.socapplication.model.dto.postMediaDto.UpdatePostMedia;
 import com.example.socapplication.service.PostMediaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,7 @@ public class PostMediaController {
         return ResponseEntity.ok(postMediaService.findAll());
     }
 
+
     @GetMapping("/{postId}/media")
     public ResponseEntity<List<ResponsePostMedia>> findByPostId(@PathVariable Long postId) {
         return ResponseEntity.ok(postMediaService.findByPostId(postId));
@@ -35,18 +37,21 @@ public class PostMediaController {
         return ResponseEntity.ok(postMediaService.findById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('SYSADMIN') and @permissionService.hasPermission(authentication, 'manage_post')")
     @PostMapping("/media")
     public ResponseEntity<Void> createPostMedia(@RequestBody AddPostMedia dto) {
         postMediaService.createPostMedia(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('SYSADMIN') and @permissionService.hasPermission(authentication, 'manage_post')")
     @PutMapping("/media/{id}")
     public ResponseEntity<Void> updatePostMedia(@PathVariable Long id, @RequestBody UpdatePostMedia dto) {
         postMediaService.updatePostMedia(id, dto);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('SYSADMIN') and @permissionService.hasPermission(authentication, 'manage_post')")
     @DeleteMapping("/media/{id}")
     public ResponseEntity<Void> deletePostMedia(@PathVariable Long id) {
         postMediaService.deletePostMedia(id);
